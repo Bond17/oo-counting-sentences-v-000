321556673fbd5a9bfb5ed9fac70bc40e126e5733
Robert Watson <spartan83@gmail.com>
2017-12-13 03:57:59 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,7 +27,8 @@ class String
   end
 
   def count_sentences
-    array = self.split
+    array = self.split(".","!","?")
+    array.each
     return (self.count(".") + self.count("!") + self.count("?"))
 
   end
